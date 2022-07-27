ac7cd31f9585c935cb43d9050dd30d188ed84914
tychiomon <plasmauser2.0@gmail.com>
2022-07-27 07:16:33 -0500

    July 25

--- popup.ts
+++ popup.ts
@@ -3,12 +3,13 @@ import 'chrome-extension-async'
 
 let removeButton = document.getElementById("removeButton");
 let editButton = document.getElementById("editButton");
+let item = <HTMLInputElement>document.getElementById("itemName");
 
 async function getActiveTabId () {
-  const [activeTab] = await chrome.tabs.query({ active: true, currentWindow: true })
-  return activeTab.id
+	const [activeTab] = await chrome.tabs.query({ active: true, currentWindow: true })
+	return activeTab.id
 }
 
 removeButton?.addEventListener("click", async() => {
-	
+	item.value = "";
 });
